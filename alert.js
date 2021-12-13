3c3014346f4a1ab46a21b560c9fb800c5b120439
Jordan Lesson <jordanlesson@Jordans-MacBook-Pro-2.local>
2021-12-13 17:50:47 -0500

    final project

---
+++ alert.js
@@ -0,0 +1,3 @@
+function alertTask() {
+    alert("Hello! This is my MI 349 Final Project. I've put in a lot of time and effort into it so I hope you like it. I've added several images and links to the page as well as using isometric design as a point of reference. In terms of layout, I used the grid method which helped out tremendously.");
+}
